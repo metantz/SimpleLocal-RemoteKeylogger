d4756d555301dd56cc845795a9e3771b14879888
Jacopo Cavallo <jacopo.cavallo@protonmail.com>
2016-05-24 01:06:19 +0200

    Update devh.c

--- devh.c
+++ devh.c
@@ -1,6 +1,6 @@
 #include "devh.h"
 
-/*to do: Write all the codes for all the keyboard layouts*/
+/*TODO: Write all the codes for all the keyboard layouts*/
 char* code_to_str(char* layout, int code)
 {
 	switch(code)
